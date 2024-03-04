0c5d09994498ad943b6db6ec0c927794992e78f3
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2024-03-04 12:23:41 +0100

    application.yml refactoring

--- src/main/java/com/videohub/videohub/VideoHubApplication.java
+++ src/main/java/com/videohub/videohub/VideoHubApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VideoHubApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VideoHubApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VideoHubApplication.class, args);
+    }
 
 }
