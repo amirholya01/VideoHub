9a6bfc52bd3ae1c6e30b11b7d3c853d6a99172b4
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2024-03-04 12:47:16 +0100

    role entity-refactor

--- src/main/java/com/videohub/videohub/domain/Role.java
+++ src/main/java/com/videohub/videohub/domain/Role.java
@@ -2,18 +2,38 @@ package com.videohub.videohub.domain;
 
 import com.videohub.videohub.enums.ERole;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+
+/**
+ * Represents a role entity in the application.
+ */
 @Data
 @Document(collection = "roles")
 public class Role {
 
+    /**
+     * The unique identifier for the role.
+     */
+    @Id
     private String id;
+
+    /**
+     * The name of the role, as defined in the ERole enum.
+     */
     private ERole name;
 
+    /**
+     * Default constructor for the Role class.
+     */
     public Role() {
     }
 
+    /**
+     * Constructor for the Role class with a parameter specifying the role name.
+     * @param name The name of the role.
+     */
     public Role(ERole name) {
         this.name = name;
     }
