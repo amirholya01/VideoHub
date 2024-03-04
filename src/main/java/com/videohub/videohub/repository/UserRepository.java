316641fd6d471e54e43b9e975ff74e1e9a157b9d
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2024-03-04 13:15:18 +0100

    refactor - repository

--- src/main/java/com/videohub/videohub/repository/UserRepository.java
+++ src/main/java/com/videohub/videohub/repository/UserRepository.java
@@ -6,9 +6,31 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+
+/**
+ * Repository interface for managing User entities in the database.
+ */
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
+
+    /**
+     * Finds a user by their username.
+     * @param username The username of the user.
+     * @return An optional containing the user with the specified username, if found.
+     */
     Optional<User> findByUsername(String username);
+
+    /**
+     * Checks if a user with the specified username exists.
+     * @param username The username to check.
+     * @return True if a user with the specified username exists, otherwise false.
+     */
     Boolean existsByUsername(String username);
+
+    /**
+     * Checks if a user with the specified email exists.
+     * @param email The email to check.
+     * @return True if a user with the specified email exists, otherwise false.
+     */
     Boolean existsByEmail(String email);
 }
--- src/main/java/com/videohub/videohub/repository/VideoRepository.java
+++ src/main/java/com/videohub/videohub/repository/VideoRepository.java
@@ -7,9 +7,31 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
+
+/**
+ * Repository interface for managing Video entities in the database.
+ */
 @Repository
 public interface VideoRepository extends MongoRepository<Video, String> {
+
+    /**
+     * Finds videos by the username of the user who uploaded them.
+     * @param user The username of the user.
+     * @return A list of videos uploaded by the user.
+     */
     List<Video> findByUserUsername(String user);
+
+    /**
+     * Finds videos by their URL.
+     * @param url The URL of the video.
+     * @return An optional containing a list of videos with the specified URL.
+     */
     Optional<List<Video>> findByUrl(String url);
+
+    /**
+     * Finds videos by the ID of the category they belong to.
+     * @param categoryId The ID of the category.
+     * @return A list of videos belonging to the category with the specified ID.
+     */
     List<Video> findByCategoryId(String categoryId);
 }
