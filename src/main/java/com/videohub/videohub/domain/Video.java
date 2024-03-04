3d4fe02d208c6169ce3edceaa0d7013457578841
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2024-03-04 12:56:08 +0100

    video entity-refactor

--- src/main/java/com/videohub/videohub/domain/Video.java
+++ src/main/java/com/videohub/videohub/domain/Video.java
@@ -6,23 +6,70 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
 
+
+/**
+ * Represents a video entity in the application.
+ */
 @Data
 @Document(collection = "videos")
 public class Video {
 
+
+    /**
+     * The unique identifier for the video.
+     */
     @Id
     private String id;
 
+    /**
+     * The title of the video.
+     */
     private String title;
+
+    /**
+     * The size of the video in bytes.
+     */
     private double size;
+
+    /**
+     * The URL where the video is stored or can be accessed.
+     */
     private String url;
+
+    /**
+     * The date when the video was created.
+     */
     private Date created;
+
+    /**
+     * The date when the video was last modified.
+     */
     private Date modified;
+
+    /**
+     * The user who uploaded the video.
+     */
     private User user;
+
+    /**
+     * The category to which the video belongs.
+     */
     private Category category;
+
+    /**
+     * Default constructor for the Video class.
+     */
     public Video() {
     }
 
+
+    /**
+     * Constructor for the Video class with parameters for user, title, size, and URL.
+     * @param user The user who uploaded the video.
+     * @param title The title of the video.
+     * @param size The size of the video in bytes.
+     * @param url The URL where the video is stored or can be accessed.
+     */
     public Video(User user, String title, double size, String url) {
         this.user = user;
         this.title = title;
@@ -32,6 +79,15 @@ public class Video {
         this.modified = new Date();
     }
 
+
+    /**
+     * Constructor for the Video class with parameters for title, size, URL, user, and category.
+     * @param title The title of the video.
+     * @param size The size of the video in bytes.
+     * @param url The URL where the video is stored or can be accessed.
+     * @param user The user who uploaded the video.
+     * @param category The category to which the video belongs.
+     */
     public Video(String title, double size, String url, User user, Category category) {
         this.title = title;
         this.size = size;
@@ -42,6 +98,11 @@ public class Video {
         this.modified = new Date();
     }
 
+
+    /**
+     * Returns a string representation of the Video object.
+     * @return A string representation of the Video object.
+     */
     @Override
     public String toString() {
         return "Video{" +
