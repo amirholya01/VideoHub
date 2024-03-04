731a06128012fd53836e1b91c64d416e283252de
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2024-03-04 12:45:16 +0100

    enum-refactor

--- src/main/java/com/videohub/videohub/enums/ERole.java
+++ src/main/java/com/videohub/videohub/enums/ERole.java
@@ -1,7 +1,22 @@
 package com.videohub.videohub.enums;
 
+/**
+ * Enum representing different roles in the application.
+ * Users can have one of the following roles: USER, MODERATOR, ADMIN.
+ */
+
 public enum ERole {
+
+    /**
+     * Represents a regular user role.
+     */
     ROLE_USER,
+    /**
+     * Represents a moderator role with additional privileges.
+     */
     ROLE_MODERATOR,
+    /**
+     * Represents an administrator role with full access and privileges.
+     */
     ROLE_ADMIN
 }
