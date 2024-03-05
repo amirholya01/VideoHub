2d2f4f0fdcf84a9887f5bf024c92979a13bc0f41
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2024-03-05 14:04:37 +0100

    refactoring-jwtProvider

--- src/main/java/com/videohub/videohub/security/jwt/JwtProvider.java
+++ src/main/java/com/videohub/videohub/security/jwt/JwtProvider.java
@@ -8,6 +8,10 @@ import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
+
+/**
+ * Component responsible for JWT (JSON Web Token) generation, validation, and retrieval of user information from the token.
+ */
 @Component
 public class JwtProvider {
 
@@ -17,20 +21,35 @@ public class JwtProvider {
     @Value("${videoHub.app.jwtExpirationMs}")
     private String jwtExpiration;
 
+
+    /**
+     * Generates a JWT token based on the provided authentication details.
+     *
+     * @param authentication The authentication object containing user details.
+     * @return The generated JWT token.
+     */
     public String generateJwtToken(Authentication authentication) {
 
         UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
 
         return Jwts.builder()
-                .setSubject((userPrincipal.getUsername()))
-                .setIssuedAt(new Date())
-                //.setExpiration(new Date((new Date()).getTime() + jwtExpiration * 1000L))
-                .signWith(SignatureAlgorithm.HS512, jwtSecret)
-                .compact();
+                .setSubject((userPrincipal.getUsername()))  // Set the subject as the username
+                .setIssuedAt(new Date())  // Set the token issue time
+                .setExpiration(new Date(System.currentTimeMillis() + Long.parseLong(jwtExpiration)))  // Set the token expiration time
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)  // Sign the token with the secret key using HS512 algorithm
+                .compact();  // Compact and return the token as a string
     }
 
+
+    /**
+     * Validates the provided JWT token.
+     *
+     * @param authToken The JWT token to validate.
+     * @return True if the token is valid, false otherwise.
+     */
     public boolean validateJwtToken(String authToken) {
         try {
+            // Parse and validate the token's signature
             Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
             return true;
         } catch (SignatureException e) {
@@ -45,10 +64,17 @@ public class JwtProvider {
         return false;
     }
 
+
+    /**
+     * Retrieves the username from the provided JWT token.
+     *
+     * @param token The JWT token.
+     * @return The username extracted from the token.
+     */
     public String getUserNameFromJwtToken(String token) {
         return Jwts.parser()
-                .setSigningKey(jwtSecret)
-                .parseClaimsJws(token)
-                .getBody().getSubject();
+                .setSigningKey(jwtSecret)  // Set the secret key for parsing
+                .parseClaimsJws(token) // Parse the token
+                .getBody().getSubject();  // Get the subject (username) from the token's body
     }
 }
